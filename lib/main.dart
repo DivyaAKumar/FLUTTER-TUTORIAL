1b81e71c46c17eec7a1f1b72dac5999f4897575f
Divya <divya24dak@gmail.com>
2025-02-12 22:10:06 +0530

    Styles and Themes in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:my_first_flutter_app/ui_helper/util.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        primarySwatch: Colors.purple,
+        textTheme: TextTheme(
+          headlineLarge: TextStyle(fontSize: 25 , fontWeight: FontWeight.bold),
+          headlineMedium:TextStyle(fontSize: 25 , fontWeight: FontWeight.bold, color: Colors.blueAccent) ,
+          titleMedium: TextStyle(fontSize: 15 , fontWeight: FontWeight.w500, fontStyle: FontStyle.italic),
+          titleSmall: TextStyle(fontSize: 15 , fontWeight: FontWeight.w500, fontStyle: FontStyle.normal, color: Colors.amber),
+        ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -35,14 +43,21 @@ class _ContainerPageState extends State<ContainerPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Custom Fonts in Flutter'),
+        title: Text('Styles and Themes in Flutter'),
         //it is used to recycle memory
         backgroundColor: Colors.purple,
       ),
-      body: //go to pubspec.yaml file and make changes in the font section below assets
-      //give the desired path of the file where the downloaded font is saved 
-      
-       Center(child: Text("Hello world", style: TextStyle(fontFamily: 'Calligraphy' , fontSize: 40 , fontWeight: FontWeight.w500),)),
+      body: Column(
+        children: [ //copy with is used to add some extra attributes and ! is placed before .copywith
+                    //to confirm that the headlineLarge is created and is not null
+          Text("Hello World", style: Theme.of(context).textTheme.headlineLarge!.copyWith(color: Colors.red)),
+          Text("Hello World", style: Theme.of(context).textTheme.titleMedium,),          
+          Text("Hello World", style: Theme.of(context).textTheme.headlineMedium),
+          Text("Hello World", style: Theme.of(context).textTheme.titleSmall),
+          Text("Hello World", style: myTextStyle40(),), //from util.dart
+          Text("Hello World", style: myTextStyle30(),), //from util.dart
+       ],
+      )
     ); //end of Scaffold
   } //end of Widget build
 } //end of _ContainerPageState
---
+++ lib/ui_helper/util.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+TextStyle myTextStyle40(
+  {
+    Color textColor = Colors.brown,
+    FontWeight fontWeight= FontWeight.normal,
+  }
+){
+  return TextStyle(
+    fontSize: 40,
+    color: textColor,
+    fontWeight: fontWeight,
+  );
+
+}
+TextStyle myTextStyle30(){
+  return TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold
+  );
+
+}
