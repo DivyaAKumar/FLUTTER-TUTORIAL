d747145a63da84a47c41ccac86da107c4a3bc769
Divya <divya24dak@gmail.com>
2025-02-14 23:25:30 +0530

    Fetching DateTime in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -42,43 +42,39 @@ class _ContainerPageState extends State<ContainerPage> {
   @override
   Widget build(BuildContext context) {
     var arr_name = ["Ram", 'Sita', 'Laxman', 'Dinesh'];
+    var time = DateTime.now();
     return Scaffold(
       appBar: AppBar(
-        title: Text('Card Widget in Flutter'),
+        title: Text('Fetching date and time in Flutter'),
         //it is used to recycle memory
         backgroundColor: Colors.purple,
       ),
-      body: //card widget is used to elevate your widget(to give it a 3d effect)
-        // Card(
-        //   elevation: 6, //5-7 or 3
-        //   shadowColor: Colors.red,
-          
-        //   child: Padding(
-        //     padding: const EdgeInsets.all(8.0),
-        //     child: Text("This is a Card Widget!", style: TextStyle(fontSize: 25),),
-        //   ),
-        //   )
-        ListView.builder(itemBuilder: (context, index) {
-          return  Card(
-            elevation: 6, //5-7 or 3
-          shadowColor: Colors.red,
-          
+      body: Center(
+        child: Container(
+          width: 500,
+          height: 500,
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              leading: Text("[$index]+1"),
-              title: Text(arr_name[index], style: TextStyle(fontSize: 25),),
-              subtitle: Text('Phone no.'),
-              trailing: Icon(Icons.accessibility),
-            )
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [    
+                Center(child: Text("Current Time in h:m:s: ${time.hour}:${time.minute}:${time.second} ", style: TextStyle(fontSize: 25),)),
+                Center(child: Text("Current day: ${time.weekday}", style: TextStyle(fontSize: 25),)),
+                Center(child: Text("Current month: ${time.month}", style: TextStyle(fontSize: 25),)),
+                Center(child: Text("Current year: ${time.year}", style: TextStyle(fontSize: 25),)),
+                Center(child: Text("Current minute: ${time.minute}", style: TextStyle(fontSize: 25),)),
+                Center(child: Text("Current Time:\n ${time}", style: TextStyle(fontSize: 25),)),
+                ElevatedButton(onPressed: (){
+                  setState(() { //calls build and fetches current time 
+                    
+                  });
             
-            
-            
-           // Text(arr_name[index], style: TextStyle(fontSize: 25),),
+                }, child: Text("Click here to view current time"))
+              ],
+            ),
           ),
-          );
-        }, itemCount: arr_name.length,
-        )
+        ),
+      )
     ); //end of Scaffold
   } //end of Widget build
 } //end of _ContainerPageState
