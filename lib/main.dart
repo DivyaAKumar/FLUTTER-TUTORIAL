78a3cf454f3752f7b94f30fab98b35e7f6b7a806
Divya <divya24dak@gmail.com>
2025-02-17 23:15:55 +0530

    Date Time Picker in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         textTheme: TextTheme(
-          headlineLarge: TextStyle(fontSize: 25 , fontWeight: FontWeight.bold),
-          headlineMedium:TextStyle(fontSize: 25 , fontWeight: FontWeight.bold, color: Colors.blueAccent) ,
-          titleMedium: TextStyle(fontSize: 15 , fontWeight: FontWeight.w500, fontStyle: FontStyle.italic),
-          titleSmall: TextStyle(fontSize: 15 , fontWeight: FontWeight.w500, fontStyle: FontStyle.normal, color: Colors.amber),
+          headlineLarge: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          headlineMedium: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.blueAccent),
+          titleMedium: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              fontStyle: FontStyle.italic),
+          titleSmall: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              fontStyle: FontStyle.normal,
+              color: Colors.amber),
         ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -42,38 +52,56 @@ class ContainerPage extends StatefulWidget {
 class _ContainerPageState extends State<ContainerPage> {
   @override
   Widget build(BuildContext context) {
-    var arr_name = ["Ram", 'Sita', 'Laxman', 'Dinesh'];
+    //var arr_name = ["Ram", 'Sita', 'Laxman', 'Dinesh'];
     var time = DateTime.now();
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Formatting Date in Flutter'),
-        //it is used to recycle memory
-        backgroundColor: Colors.purple,
-      ),
-      body: Center(
-        child: Container(
-          width: 500,
-          height: 500,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
+        appBar: AppBar(
+          title: Text('Date Time Picker in Flutter'),
+          //it is used to recycle memory
+          backgroundColor: Colors.purple,
+        ),
+        body: Center(
+          child: Container(
+            width: 400,
+            height: 200,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [    
-                
-                Center(child: Text("Current Time in hms:\n ${DateFormat('Hms').format(time)}", style: TextStyle(fontSize: 25),)),
-                 Center(child: Text("Current Time in yMMMd:\n ${DateFormat('yMMMd').format(time)}", style: TextStyle(fontSize: 25),)),
-                 Center(child: Text("Current Time in MMMMEEEEd:\n ${DateFormat('MMMMEEEEd').format(time)}", style: TextStyle(fontSize: 25),)),
-                 ElevatedButton(onPressed: (){
-                  setState(() { //calls build and fetches current time 
-                    
-                  });
-            
-                }, child: Text("Click here to view current time"))
-              ],
+              children: [
+                Text(
+                  "Select Date and Time",
+                  style: TextStyle(fontSize: 25),
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      DateTime? datePicked = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2020),
+                          lastDate: DateTime(2026));
+                      if (datePicked != null) {
+                        print(
+                            "Date Selected: ${datePicked.day}- ${datePicked.month}- ${datePicked.year}");
+                      }
+                    },
+                    child: Text("Select Date")),
+
+                //FOR SELECTING TIME
+                ElevatedButton(
+                    onPressed: () async {
+                      TimeOfDay? pickedTime = await showTimePicker(
+                          context: context,
+                          initialTime: TimeOfDay.now(),
+                          initialEntryMode: TimePickerEntryMode.dial //or input
+                          );
+                      if (pickedTime != null) {
+                        print(
+                            "Time Selected: ${pickedTime.hour}:${pickedTime.minute}");
+                      }
+                    },
+                    child: Text("Select Time")),
+              ], //children 1
             ),
           ),
-        ),
-      )
-    ); //end of Scaffold
+        )); //end of Scaffold
   } //end of Widget build
 } //end of _ContainerPageState
