e4c0f4162f26b68741e5e6eeffd73e8140edfc39
Divya <divya24dak@gmail.com>
2025-02-18 21:06:25 +0530

    TextField in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -16,26 +16,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.purple,
-        textTheme: TextTheme(
-          headlineLarge: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          headlineMedium: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-              color: Colors.blueAccent),
-          titleMedium: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w500,
-              fontStyle: FontStyle.italic),
-          titleSmall: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w500,
-              fontStyle: FontStyle.normal,
-              color: Colors.amber),
-        ),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        appBarTheme: AppBarTheme(
+        backgroundColor: Colors.blue, 
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 20), 
+  ),
       ),
       home: ContainerPage(),
     );
@@ -50,58 +37,106 @@ class ContainerPage extends StatefulWidget {
 }
 
 class _ContainerPageState extends State<ContainerPage> {
+  var emailText= TextEditingController();
+  var passwordText = TextEditingController();
   @override
   Widget build(BuildContext context) {
     //var arr_name = ["Ram", 'Sita', 'Laxman', 'Dinesh'];
     var time = DateTime.now();
     return Scaffold(
         appBar: AppBar(
-          title: Text('Date Time Picker in Flutter'),
-          //it is used to recycle memory
-          backgroundColor: Colors.purple,
+          title: Text('TextField in Flutter(Text Input Widget)'),
+         // backgroundColor: Colors.purple,
         ),
         body: Center(
           child: Container(
-            width: 400,
-            height: 200,
+            width: 300,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  "Select Date and Time",
-                  style: TextStyle(fontSize: 25),
-                ),
-                ElevatedButton(
-                    onPressed: () async {
-                      DateTime? datePicked = await showDatePicker(
-                          context: context,
-                          initialDate: DateTime.now(),
-                          firstDate: DateTime(2020),
-                          lastDate: DateTime(2026));
-                      if (datePicked != null) {
-                        print(
-                            "Date Selected: ${datePicked.day}- ${datePicked.month}- ${datePicked.year}");
-                      }
-                    },
-                    child: Text("Select Date")),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: emailText,
+                  //enabled: false,
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Email",
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(11),
+                      borderSide: BorderSide(
+                        color: Colors.orange,
+                        width: 3
+                      )
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(11),
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 2,
+                      )
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(11),
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                        width: 2,
+                      )
+                    ),
+                    //suffixText: "Hint: Your dog's name.",
+                    suffixIcon: IconButton(onPressed: () { }, 
+                    icon: Icon(Icons.admin_panel_settings_outlined, color: Colors.blue,)
+                    ),
+                    prefixIcon: Icon(Icons.email , color: Colors.blue,)
+                    // border: OutlineInputBorder(
+                    //   borderRadius: BorderRadius.circular(11),
+                    //   borderSide: BorderSide(
+                    //     color: Colors.black
+                    //   )
+                    // )
 
-                //FOR SELECTING TIME
-                ElevatedButton(
-                    onPressed: () async {
-                      TimeOfDay? pickedTime = await showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay.now(),
-                          initialEntryMode: TimePickerEntryMode.dial //or input
-                          );
-                      if (pickedTime != null) {
-                        print(
-                            "Time Selected: ${pickedTime.hour}:${pickedTime.minute}");
-                      }
-                    },
-                    child: Text("Select Time")),
-              ], //children 1
+                  )
+                ),
+                Container(
+                  height: 11,
+                ),
+                 TextField(
+                  keyboardType: TextInputType.number,
+                  controller: passwordText,
+                  obscureText: true, // to hide password
+                  obscuringCharacter: "*", //character to use for hiding the password
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Password",
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(11),
+                      borderSide: BorderSide(
+                        color: Colors.orange,
+                        width: 2,
+                      )
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(11),
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 2
+                      )
+                    ) ,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(11),
+                      borderSide: BorderSide(
+                        color: Colors.black,
+                        width: 2,
+                      )
+                    )
+                  )
+                ),
+                ElevatedButton(onPressed: () {
+                  String userEmail = emailText.text.toString();
+                  String userPwd = passwordText.text.toString();
+                  print("Email: $userEmail and Password: $userPwd");
+                }, child: Text("Login"))
+              ],
             ),
           ),
-        )); //end of Scaffold
+        )
+        ); //end of Scaffold
   } //end of Widget build
 } //end of _ContainerPageState
