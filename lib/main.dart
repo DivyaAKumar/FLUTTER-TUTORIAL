19c5168bee407da7a5dd5619287dca06da9903ff
Divya <divya24dak@gmail.com>
2025-02-27 11:41:26 +0530

    List view scrollview

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         appBarTheme: AppBarTheme(
-        backgroundColor: Colors.blue, 
-        titleTextStyle: TextStyle(color: Colors.white, fontSize: 20), 
-  ),
+          backgroundColor: Colors.blue,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+        ),
       ),
       home: MyHomePage(),
     );
@@ -35,71 +35,159 @@ class MyApp extends StatelessWidget {
 } //end of MyApp class
 
 class MyHomePage extends StatefulWidget {
- 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-   var _width = 200.0; //to create pvt field inside dart _ is used before name
+  var _width = 200.0; //to create pvt field inside dart _ is used before name
   var _height = 100.0;
- var my_opacity =1.0;
- bool isFirst =true;
+  var my_opacity = 1.0;
+  bool isFirst = true;
+  var arrIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,14,15,16];
 
- bool isVisible =true;
-   bool flag =true;
-   Color bgColor = Colors.blueGrey;
-   Decoration myDecor = BoxDecoration(
-    borderRadius: BorderRadius.circular(2)
-   );
-  
-   @override
+  bool isVisible = true;
+  bool flag = true;
+  Color bgColor = Colors.blueGrey;
+  //Decoration myDecor = BoxDecoration(borderRadius: BorderRadius.circular(2));
+
+  @override
   //  void initState() {
-    
+
   //   super.initState();
   //   Timer(Duration(seconds: 4),() {
   //     reload();
   //   },);
   //  }
-    void reload(){
-      setState(() {
-       if (isFirst) {
-          isFirst=false;
-       } else {
-          isFirst=true;
-       }
-      });
-    }
-  
+  void reload() {
+    setState(() {
+      if (isFirst) {
+        isFirst = false;
+      } else {
+        isFirst = true;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
- 
     return Scaffold(
-        appBar: AppBar(
-          
-          title: Text('Hero Animation(V.Imp)'),
-           
-        ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: const Color.fromARGB(255, 50, 6, 170),
-        child: Center(
-          child: InkWell(
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => Detailedpage(),));
-            },
-            child: Hero(
-              tag: 'disney', 
-              child: Image.asset("assets/images/Disney.png", width: 200,height: 100,)
-              
-              ),
-          ),
-        ),
-      )
+      appBar: AppBar(
+        title: Text('List view scrollview'),
+      ),
+      body: Center(
+        child: ListWheelScrollView(
+          itemExtent: 200,
+          children: arrIndex.map((value) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: Center(child: Text('$value', style: TextStyle(fontSize: 25),)),
+                      height: double.infinity,
+                      decoration: BoxDecoration(
+                         color: Colors.blue,
+                         borderRadius: BorderRadius.circular(21)
+                      ),
+                     
+                    ),
+          ))
         
-        ); //end of Scaffold
+              .toList(), // Ensure it's a List<Widget>
+        ),
+      ),
+
+      // Container(
+      //   height: 100,
+      //   width: double.infinity,
+      //   color: Colors.deepPurpleAccent,
+      //   // decoration: BoxDecoration(
+
+      //   // ),
+
+      // ),
+      // Container(
+      //   height: 100,
+      //   width: double.infinity,
+      //   color: Colors.blueGrey,
+      //   // decoration: BoxDecoration(
+
+      //   // ),
+
+      // ),Container(
+      //   height: 100,
+      //   width: double.infinity,
+      //   color: Colors.orange,
+      //   // decoration: BoxDecoration(
+
+      //   // ),
+
+      // ),Container(
+      //   height: 100,
+      //   width: double.infinity,
+      //   color: Colors.red,
+      //   // decoration: BoxDecoration(
+
+      //   // ),
+
+      // ),Container(
+      //   height: 100,
+      //   width: double.infinity,
+      //   color: Colors.pink,
+      //   // decoration: BoxDecoration(
+
+      //   // ),
+
+      // ),
+      // Container(
+      //   height: 100,
+      //   width: double.infinity,
+      //   color: Colors.purple,
+      //   // decoration: BoxDecoration(
+
+      //   // ),
+
+      // ),
+      // Container(
+      //   height: 100,
+      //   width: double.infinity,
+      //   color: Colors.green,
+      //   // decoration: BoxDecoration(
+
+      //   // ),
+
+      // ),
+      // Container(
+      //   height: 100,
+      //   width: double.infinity,
+      //   color: Colors.blue,
+      //   // decoration: BoxDecoration(
+
+      //   // ),
+
+      // ),
+      // Container(
+      //   height: 100,
+      //   width: double.infinity,
+      //   color: Colors.orange,
+      //   // decoration: BoxDecoration(
+
+      //   // ),
+
+      // ),
+      // Container(
+      //   height: 100,
+      //   width: double.infinity,
+      //   color: Colors.red,
+
+      // ),
+      // Container(
+      //   height: 100,
+      //   width: double.infinity,
+      //   color: Colors.amber,
+      //   // decoration: BoxDecoration(
+
+      //   // ),
+
+      // ),
+    ); //end of Scaffold
   } //end of Widget build
 } //end of _ContainerPageState
-
-
