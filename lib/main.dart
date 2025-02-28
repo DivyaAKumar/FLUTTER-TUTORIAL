172be831c7bfeced820e81a30d198ab152f934a0
Divya <divya24dak@gmail.com>
2025-02-28 11:09:10 +0530

    Tween Animation

--- lib/main.dart
+++ lib/main.dart
@@ -39,78 +39,32 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  var _width = 200.0; //to create pvt field inside dart _ is used before name
-  var _height = 100.0;
-  var my_opacity = 1.0;
-  bool isFirst = true;
-  var arrIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-
-  var arrData = [
-    {'name': 'Alice', 'mobno': '7898114074', 'unread': '1'},
-    {'name': 'Michael', 'mobno': '9234567890', 'unread': '5'},
-    {'name': 'Sophia', 'mobno': '8123456789', 'unread': '2'},
-    {'name': 'James', 'mobno': '7567891234', 'unread': '3'},
-    {'name': 'Alice', 'mobno': '7898114074', 'unread': '1'},
-    {'name': 'Michael', 'mobno': '9234567890', 'unread': '5'},
-    {'name': 'Sophia', 'mobno': '8123456789', 'unread': '2'},
-    {'name': 'James', 'mobno': '7567891234', 'unread': '3'},
-    {'name': 'Alice', 'mobno': '7898114074', 'unread': '1'},
-    {'name': 'Michael', 'mobno': '9234567890', 'unread': '5'},
-    {'name': 'Sophia', 'mobno': '8123456789', 'unread': '2'},
-    {'name': 'James', 'mobno': '7567891234', 'unread': '3'},
-    {'name': 'Alice', 'mobno': '7898114074', 'unread': '1'},
-    {'name': 'Michael', 'mobno': '9234567890', 'unread': '5'},
-    {'name': 'Sophia', 'mobno': '8123456789', 'unread': '2'},
-    {'name': 'James', 'mobno': '7567891234', 'unread': '3'},
-
-  ];
-
-//   var arrData1 = [
-//     {
-//      'name': 'Alice',
-//      'mobno': '7898114074',
-//      'unread' : '1',
-//     },
-
-// 'Alice',
-// 'Michael',
-// 'Sophia',
-// 'James',
-// 'Olivia',
-// 'Daniel',
-// 'Isabella',
-// 'Ethan',
-// 'Emma',
-// 'Alexander',
-// 'Mia',
-// 'Benjamin',
-// 'Charlotte',
-// 'William',
-// 'Amelia',
-
-//   ];
-  bool isVisible = true;
-  bool flag = true;
-  Color bgColor = Colors.blueGrey;
-  //Decoration myDecor = BoxDecoration(borderRadius: BorderRadius.circular(2));
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
+  late Animation animation; //baad me initialize karne ke liye late keyword
+  late Animation colorAnimation;
+  late AnimationController animationController;
+  
 
   @override
-  //  void initState() {
+  
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController = AnimationController(vsync: this, duration: Duration( seconds: 5));
+    animation = Tween(begin: 250.0,end: 100.0).animate(animationController); //make sure to give double 
+    
+    
+    colorAnimation = ColorTween(begin: Colors.indigo,end: Colors.red).animate(animationController);
+    animationController.addListener(() {
+      print(animation.value);
+      setState(() {
+        
+      
+      });
+    },);
 
-  //   super.initState();
-  //   Timer(Duration(seconds: 4),() {
-  //     reload();
-  //   },);
-  //  }
-  void reload() {
-    setState(() {
-      if (isFirst) {
-        isFirst = false;
-      } else {
-        isFirst = true;
-      }
-    });
+    animationController.forward(); //to start animation 
+   
   }
 
   @override
@@ -118,44 +72,18 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
         appBar: AppBar(
           title:
-              Text('Mapping lists to widgets'), //MORE FROM WEB GRADIENTS SITE
-        ),
-        // (0,0)......(1,0)
-        // (0,1).......(1,1)
-        body: Container(
-            child: ListView(
-          children: arrData
-              .map((value) => ListTile(
-                    leading: Icon(Icons.account_circle),
-                    title: Text(value['name']
-                        .toString()), 
-                    subtitle: Text(value['mobno'].toString()),
-                    trailing: CircleAvatar(
-                      radius: 12,
-                      backgroundColor: Colors.blue[200],
-                      child: Text(value['unread'].toString(),style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black87),)
-                      ),
-                  ))
-              .toList(),
+              Text('Tween animation'), //between(begin and end specify), 
+        ), //AppBar            //2.controller-controls animation   
+                       //mixin class-(single tick provider)-gives main points b/w begin and end-gives values(timing duration divide) to apply animation 
+
+
+        body: Center(
+          child: Container(
+            width: animation.value,
+            height: animation.value,
+            color: colorAnimation.value,
+          ),
         )
-
-            //   // child: ListView(
-            //   //   children: arrData.map((value) {
-            //   //     return Padding(
-            //   //       padding: const EdgeInsets.all(8.0),
-            //   //       child: Container(
-            //   //         decoration: BoxDecoration(
-            //   //           borderRadius: BorderRadius.circular(20),
-            //   //            color: Colors.blueAccent,
-            //   //         ),
-            //   //         width: double.infinity,
-            //   //         height: 100,
-            //   //         child: Center(child: Text(${value}, style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),), ),
-
-            //   //       ),
-            //   //     );
-            //   //   },).toList()
-            // ),
-            )); //end of Scaffold
+            ); //end of Scaffold
   } //end of Widget build
 } //end of _ContainerPageState
